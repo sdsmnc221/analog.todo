bdb2a7f4423453fa89be13c13d56da8c04a0df03
sdsmnc221 <antr.2201@gmail.com>
2025-05-22 16:31:50 +0200

    feat(app): oyyy init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import "./styles/index.scss";
+</script>
+
+<template>
+  <div class="text-6xl text-red-500 font-bold underline">TODO</div>
+</template>
+
+<style scoped></style>
